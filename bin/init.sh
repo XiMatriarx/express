7b8af15f44520cbc9766856d8f0af76c9608ad20
Xi Matriarx <xi@ximatriarx.io>
2024-02-14 19:13:48 +0200

    init (#1)

---
+++ bin/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+read -p "Project name: " project
+
+if [ -z "$project" ]
+then
+	echo "Project name is required."
+	exit
+fi
+
+git clone https://github.com/XiMatriarx/express.git --depth 1 --branch 'v0.0.1'
+mv express $project
+cd $project
+rm -Rf .git
+rm -Rf bin
+npm install
