a4dc1205061bbdb6058147db94ea58b00b3e3c42
Xi Matriarx <xi@ximatriarx.io>
2024-02-18 15:54:58 +0200

    version

--- bin/init.sh
+++ bin/init.sh
@@ -8,7 +8,7 @@ then
 	exit
 fi
 
-git clone https://github.com/XiMatriarx/express.git --depth 1 --branch 'v1.0.1'
+git clone https://github.com/XiMatriarx/express.git --depth 1 --branch 'v1.0.2'
 mv express $project
 cd $project
 rm -Rf .git
